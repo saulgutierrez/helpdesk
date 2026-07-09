817a1d743c2033fbdc895ee4a2960549deb669af
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2026-07-08 21:48:46 -0600

    first commit

---
+++ config/conexion.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+
+    class Conectar {
+        protected $dbh;
+
+        protected function Conexion() {
+            try {
+                $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=helpdesk","root","");
+                return $conectar;
+            } catch (Exception $e) {
+                print "¡Error DB!: ". $e->getMessage() . "<br/>";
+                die();
+            }
+        }
+
+        public function set_names() {
+            return $this->dbh->query("SET NAMES 'utf8'");
+        }
+
+        public function ruta() {
+            return "http://localhost/helpdesk/";
+        }
+    }
+?>
