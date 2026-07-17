2075cbccd0ecf7ad1f51ab4264d0bf84c6138b7b
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2026-07-16 18:14:27 -0600

    Maquetación básica de proyecto

---
+++ view/ConsultarTicket/index.php
@@ -0,0 +1,1 @@
+<h1>Consultar Ticket</h1>
---
+++ view/Home/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <?php require_once("../MainHead/head.php"); ?>
+	<title>Home</title>
+</head>
+<body class="with-side-menu">
+
+	<?php require_once("../MainHeader/header.php"); ?>
+
+	<div class="mobile-menu-left-overlay"></div>
+
+    <?php require_once("../MainNav/nav.php"); ?>
+
+	<div class="page-content">
+		<div class="container-fluid">
+			Blank page.
+		</div><!--.container-fluid-->
+	</div><!--.page-content-->
+
+	<?php require_once("../MainJs/js.php"); ?>
+</body>
+</html>
